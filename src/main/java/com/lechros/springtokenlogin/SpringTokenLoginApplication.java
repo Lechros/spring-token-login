97d19b16358c39fde68269e75c4f159e5c885d56
Lechros <0onionion0@gmail.com>
2024-03-08 22:17:16 +0900

    init

---
+++ src/main/java/com/lechros/springtokenlogin/SpringTokenLoginApplication.java
@@ -0,0 +1,13 @@
+package com.lechros.springtokenlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTokenLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTokenLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lechros/springtokenlogin/SpringTokenLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lechros.springtokenlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTokenLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
