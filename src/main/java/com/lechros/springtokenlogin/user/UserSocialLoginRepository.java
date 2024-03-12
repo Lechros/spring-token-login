f0203e5fdff5758ddceffbea2a611813a58ce900
Lechros <0onionion0@gmail.com>
2024-03-13 02:19:36 +0900

    feat: UserSocialLoginRepository 추가

---
+++ src/main/java/com/lechros/springtokenlogin/user/UserSocialLoginRepository.java
@@ -0,0 +1,10 @@
+package com.lechros.springtokenlogin.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserSocialLoginRepository extends JpaRepository<UserSocialLogin, Long> {
+
+    Optional<UserSocialLogin> findByProviderAndProviderId(String provider, String providerId);
+}
