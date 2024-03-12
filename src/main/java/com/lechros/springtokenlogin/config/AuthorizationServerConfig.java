54fe65f135dc6c27e1719fffd4e2322c6a3cf44f
Lechros <0onionion0@gmail.com>
2024-03-13 00:23:17 +0900

    fix: SecurityFilterChain이 소셜 로그인 요청에만 동작하도록 수정

--- src/main/java/com/lechros/springtokenlogin/config/AuthorizationServerConfig.java
+++ src/main/java/com/lechros/springtokenlogin/config/AuthorizationServerConfig.java
@@ -4,18 +4,37 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRedirectFilter;
+import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class AuthorizationServerConfig {
 
+    /**
+     * 소셜 로그인 처리 URI
+     */
+    private static String authorizationUri =
+        OAuth2AuthorizationRequestRedirectFilter.DEFAULT_AUTHORIZATION_REQUEST_BASE_URI;
+    /**
+     * 인증 요청 처리 URI
+     */
+    private static String redirectionUri =
+        OAuth2LoginAuthenticationFilter.DEFAULT_FILTER_PROCESSES_URI;
+
     @Bean
     @Order(Ordered.HIGHEST_PRECEDENCE)
     public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
         http
-            .oauth2Login(Customizer.withDefaults());
+            // SecurityFilterChain에서 소셜 로그인, 인증 요청 URI만 처리 -> 로그인 페이지 제거
+            .securityMatcher(authorizationUri + "/*", redirectionUri)
+            // 위 securityMatcher와 일치하도록 oauth2Login 설정 반영
+            .oauth2Login(oauth2Login -> oauth2Login
+                .authorizationEndpoint(authorization -> authorization
+                    .baseUri(authorizationUri))
+                .redirectionEndpoint(redirection -> redirection
+                    .baseUri(redirectionUri)));
 
         return http.build();
     }
