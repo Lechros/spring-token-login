49d16bcf89ac366defb0dcffa764e818267e21a5
Lechros <0onionion0@gmail.com>
2024-03-12 23:07:18 +0900

    feat: 카카오 로그인 기본 구현

---
+++ src/main/java/com/lechros/springtokenlogin/config/AuthorizationServerConfig.java
@@ -0,0 +1,22 @@
+package com.lechros.springtokenlogin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class AuthorizationServerConfig {
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .oauth2Login(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
