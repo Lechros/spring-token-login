6ec151ddaad333479ea520ebf9b417fdeb2e0653
Lechros <0onionion0@gmail.com>
2024-03-13 02:33:36 +0900

    feat: AccessTokenResponse 추가

---
+++ src/main/java/com/lechros/springtokenlogin/token/AccessTokenResponse.java
@@ -0,0 +1,49 @@
+package com.lechros.springtokenlogin.token;
+
+import lombok.*;
+import org.springframework.security.oauth2.core.AbstractOAuth2Token;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class AccessTokenResponse {
+
+    private String tokenType;
+    private String accessToken;
+    private Long expiresIn;
+    private String refreshToken;
+    private Long refreshTokenExpiresIn;
+
+    public static Builder withToken(AbstractOAuth2Token accessToken) {
+        return new Builder().accessToken(accessToken);
+    }
+
+    public static class Builder {
+
+        private String tokenType;
+        private String accessToken;
+        private Long expiresIn;
+        private String refreshToken;
+        private Long refreshTokenExpiresIn;
+
+        Builder accessToken(AbstractOAuth2Token token) {
+            tokenType = OAuth2AccessToken.TokenType.BEARER.getValue();
+            accessToken = token.getTokenValue();
+            expiresIn = Instant.now().until(token.getExpiresAt(), ChronoUnit.SECONDS);
+            return this;
+        }
+
+        public Builder refreshToken(AbstractOAuth2Token token) {
+            refreshToken = token.getTokenValue();
+            refreshTokenExpiresIn = Instant.now().until(token.getExpiresAt(), ChronoUnit.SECONDS);
+            return this;
+        }
+
+        public AccessTokenResponse build() {
+            return new AccessTokenResponse(tokenType, accessToken, expiresIn, refreshToken, refreshTokenExpiresIn);
+        }
+    }
+}
