33bc8afaf1f04b55a36527d664fc029ca009574b
Lechros <0onionion0@gmail.com>
2024-03-13 10:07:39 +0900

    feat: IssuedRefreshToken invalidate, from 추가

--- src/main/java/com/lechros/springtokenlogin/token/IssuedRefreshToken.java
+++ src/main/java/com/lechros/springtokenlogin/token/IssuedRefreshToken.java
@@ -2,12 +2,16 @@ package com.lechros.springtokenlogin.token;
 
 import com.lechros.springtokenlogin.user.User;
 import jakarta.persistence.*;
-import lombok.Getter;
+import lombok.*;
+import org.springframework.security.oauth2.core.AbstractOAuth2Token;
 
 import java.time.Instant;
 
 @Entity
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
 public class IssuedRefreshToken {
 
     @Id
@@ -31,10 +35,27 @@ public class IssuedRefreshToken {
         return tokenValue.equals(token) && user.getId().equals(userId) && checkTime(Instant.now()) && !getInvalidated();
     }
 
+    public void invalidate() {
+        if (invalidated) {
+            throw new RuntimeException("Already invalidated");
+        }
+        invalidated = true;
+    }
+
     public boolean checkTime(Instant now) {
         // 발급과 검증을 동일한 서버에서 진행하므로 clock skew를 고려할 필요가 없음
         Instant iat = Instant.ofEpochSecond(issuedAt);
         Instant exp = Instant.ofEpochSecond(expiresAt);
         return now.isAfter(iat) && now.isBefore(exp);
+    }
+
+    public static IssuedRefreshToken from(AbstractOAuth2Token token, User user) {
+        return IssuedRefreshToken.builder()
+                .tokenValue(token.getTokenValue())
+                .user(user)
+                .issuedAt(token.getIssuedAt().getEpochSecond())
+                .expiresAt(token.getExpiresAt().getEpochSecond())
+                .invalidated(false)
+                .build();
     }
 }
