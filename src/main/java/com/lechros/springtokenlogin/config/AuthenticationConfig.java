aff26853a0e13a8bf00b14808f47d69ead61a3cd
Lechros <0onionion0@gmail.com>
2024-03-13 17:18:21 +0900

    feat: fetch로 code 요청 지원
    = InMemoryOAuth2AuthorizationRequestRepository 구현 사용
    - client url 설정 추가

--- src/main/java/com/lechros/springtokenlogin/config/AuthenticationConfig.java
+++ src/main/java/com/lechros/springtokenlogin/config/AuthenticationConfig.java
@@ -1,13 +1,20 @@
 package com.lechros.springtokenlogin.config;
 
+import com.lechros.springtokenlogin.authentication.InMemoryOAuth2AuthorizationRequestRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRedirectFilter;
 import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class AuthenticationConfig {
@@ -23,6 +30,9 @@ public class AuthenticationConfig {
     private static String redirectionUri =
         OAuth2LoginAuthenticationFilter.DEFAULT_FILTER_PROCESSES_URI;
 
+    @Value("${client.base-url}")
+    private String clientBaseUrl;
+
     @Bean
     @Order(Ordered.HIGHEST_PRECEDENCE)
     public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -32,10 +42,30 @@ public class AuthenticationConfig {
             // 위 securityMatcher와 일치하도록 oauth2Login 설정 반영
             .oauth2Login(oauth2Login -> oauth2Login
                 .authorizationEndpoint(authorization -> authorization
-                    .baseUri(authorizationUri))
+                    .baseUri(authorizationUri)
+                    .authorizationRequestRepository(authorizationRequestRepository()))
                 .redirectionEndpoint(redirection -> redirection
-                    .baseUri(redirectionUri)));
+                    .baseUri(redirectionUri)))
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()));
 
         return http.build();
+    }
+
+    @Bean
+    public AuthorizationRequestRepository<OAuth2AuthorizationRequest> authorizationRequestRepository() {
+        return new InMemoryOAuth2AuthorizationRequestRepository();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin(clientBaseUrl);
+        config.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
