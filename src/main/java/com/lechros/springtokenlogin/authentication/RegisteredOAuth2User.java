a67521e2f6f7d136059854313f1f41f07bdcc997
Lechros <0onionion0@gmail.com>
2024-03-14 09:08:54 +0900

    fix: DB, Hibernate 설정 수정

--- src/main/java/com/lechros/springtokenlogin/authentication/RegisteredOAuth2User.java
+++ src/main/java/com/lechros/springtokenlogin/authentication/RegisteredOAuth2User.java
@@ -1,30 +1,40 @@
 package com.lechros.springtokenlogin.authentication;
 
 import com.lechros.springtokenlogin.user.User;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
-import java.util.Collection;
-import java.util.Map;
+import java.util.*;
 
-public class RegisteredOAuth2User extends DefaultOAuth2User {
+@Getter
+@EqualsAndHashCode
+@ToString
+public class RegisteredOAuth2User implements OAuth2User {
 
-    private final String name;
+    private final Set<GrantedAuthority> authorities;
 
-    @Getter
+    private final Map<String, Object> attributes;
+
     private final User user;
 
     private RegisteredOAuth2User(Collection<? extends GrantedAuthority> authorities, Map<String, Object> attributes, User user) {
-        super(authorities, attributes, "");
-        this.name = String.valueOf(user.getId());
+        this.authorities = authorities != null ? Collections.unmodifiableSet(new LinkedHashSet<>(this.sortAuthorities(authorities))) : Collections.unmodifiableSet(new LinkedHashSet<>(AuthorityUtils.NO_AUTHORITIES));
+        this.attributes = Collections.unmodifiableMap(new LinkedHashMap<>(attributes));
         this.user = user;
     }
 
-    @Override
     public String getName() {
-        return name;
+        return user.getId().toString();
+    }
+
+    private Set<GrantedAuthority> sortAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        SortedSet<GrantedAuthority> sortedAuthorities = new TreeSet<>(Comparator.comparing(GrantedAuthority::getAuthority));
+        sortedAuthorities.addAll(authorities);
+        return sortedAuthorities;
     }
 
     public static RegisteredOAuth2User from(OAuth2User oauth2User, User user) {
