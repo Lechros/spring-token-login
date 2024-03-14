02e445a973e5eebce16ed45605aeba229f28a0a4
Lechros <0onionion0@gmail.com>
2024-03-14 12:30:37 +0900

    feat: jwt 시큐리티 설정

--- src/main/java/com/lechros/springtokenlogin/config/SecurityConfig.java
+++ src/main/java/com/lechros/springtokenlogin/config/SecurityConfig.java
@@ -1,20 +1,35 @@
 package com.lechros.springtokenlogin.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CorsConfigurationSource corsConfigurationSource;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(authorize -> authorize
-                .anyRequest().authenticated());
+                // 토큰 갱신 요청 경로 (TokenController)
+                .requestMatchers(HttpMethod.POST, "/oauth2/token").permitAll()
+                .anyRequest().authenticated())
+            .oauth2ResourceServer(oauth2 -> oauth2
+                .jwt(Customizer.withDefaults()))
+            .cors(cors -> cors.configurationSource(corsConfigurationSource))
+
+            .formLogin(formLogin -> formLogin.disable())
+            .csrf(csrf -> csrf.disable());
 
         return http.build();
     }
