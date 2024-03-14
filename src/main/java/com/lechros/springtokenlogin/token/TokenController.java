6540ed319130e9e62e9d13e5dffde80f7f9de926
Lechros <0onionion0@gmail.com>
2024-03-14 12:27:14 +0900

    feat: 토큰 갱신 API 추가

---
+++ src/main/java/com/lechros/springtokenlogin/token/TokenController.java
@@ -0,0 +1,27 @@
+package com.lechros.springtokenlogin.token;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class TokenController {
+
+    private final TokenService tokenService;
+
+    @PostMapping(value = "/oauth2/token", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public AccessTokenResponse token(
+        @RequestParam("grant_type") String grantType,
+        @RequestParam("refresh_token") String refreshToken
+    ) {
+        if (!OAuth2ParameterNames.REFRESH_TOKEN.equals(grantType)) {
+            throw new RuntimeException("unsupported_grant_type");
+        }
+
+        return tokenService.refreshAccessToken(refreshToken);
+    }
+}
