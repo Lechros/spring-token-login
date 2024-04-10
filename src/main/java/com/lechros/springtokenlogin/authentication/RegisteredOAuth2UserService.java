9a307aa2fb73c4dcfd75184d6cfac57e79371570
Lechros <0onionion0@gmail.com>
2024-04-10 18:17:12 +0900

    refactor: delegate based 전략 사용

--- src/main/java/com/lechros/springtokenlogin/authentication/RegisteredOAuth2UserService.java
+++ src/main/java/com/lechros/springtokenlogin/authentication/RegisteredOAuth2UserService.java
@@ -7,6 +7,7 @@ import com.lechros.springtokenlogin.user.UserSocialLoginRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
@@ -15,15 +16,17 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class RegisteredOAuth2UserService extends DefaultOAuth2UserService {
+public class RegisteredOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 
     private final UserRepository userRepository;
     private final UserSocialLoginRepository userSocialLoginRepository;
 
+    private final DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
+
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         // 토큰 정보 API에서 사용자 정보 받아오기
-        OAuth2User oauth2User = super.loadUser(userRequest);
+        OAuth2User oauth2User = delegate.loadUser(userRequest);
         // 받아온 정보의 sub로 필요 시 DB에 저장하고 user 가져오기
         User user = findOrRegister(userRequest, oauth2User.getName());
         // name이 user.id(PK)로 설정된 OAuth2User 반환
